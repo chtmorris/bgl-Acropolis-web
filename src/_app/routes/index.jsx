18f6f9252c6a2eb6753632283271433c964951c1
jbarinas <jbarinas@kobo.com>
2017-04-22 10:01:18 -0400

    adding(views)

--- src/_app/routes/index.jsx
+++ src/_app/routes/index.jsx
@@ -4,16 +4,22 @@ import {Route, IndexRoute, Redirect} from 'react-router';
 import Layout from '_app/_base/components/layouts/Layout';
 import NoMatch from '_app/_base/views/NoMatch';
 import Nav from '_app/containers/Nav';
-import Search from '_app/views/Search';
+import Home from '_app/views/Home/components';
+import Profile from '_app/views/Profile/components';
 
 const Routes = [(<Route path='/404' component={NoMatch}/>), (
         <Route path='/' component={Layout}>
             <IndexRoute components={{
                 nav: Nav,
-                main: Search
+                main: Home
             }}/>
 
-            <Redirect from='/search' to='/'/>
+            <Route path="profile" components={{
+                nav: Nav,
+                main: Profile
+            }}/>
+
+            <Redirect from='/home' to='/'/>
             <Redirect from='*' to='/404'/>
         </Route>
     )];
