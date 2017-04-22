74be7dd4116198a302887273970a2222edc6680c
chtmorris <agoosester@gmail.com>
2017-04-22 10:38:27 -0400

    added house asset and card

--- src/_app/views/Profile/components/index.jsx
+++ src/_app/views/Profile/components/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import style from './index.css';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Profile extends React.Component {
     render() {
         return (
-            <div>profile</div>
+            <div className={style['root']}>profile
+              <Card>
+                <CardHeader
+                  title="URL Avatar"
+                  subtitle="Subtitle"
+                  avatar="images/jsa-128.jpg"
+                />
+                <CardMedia
+                  overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                >
+                  <img src="/assets/img/house.png" />
+                </CardMedia>
+                <CardTitle title="Why invest with Joan Wagner" subtitle="Card subtitle" />
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+                <CardActions>
+                  <FlatButton label="Action1" />
+                  <FlatButton label="Action2" />
+                </CardActions>
+              </Card>
+            </div>
         );
     }
 }
