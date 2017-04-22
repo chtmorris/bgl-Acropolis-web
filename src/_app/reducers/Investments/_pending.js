1b240302fc2a67314e27a1117908259efd4f2aa1
jbarinas <jbarinas@kobo.com>
2017-04-22 14:45:10 -0400

    update(data)

--- src/_app/reducers/Investments/_pending.js
+++ src/_app/reducers/Investments/_pending.js
@@ -7,6 +7,24 @@ let list = [
         email_address: 'jgrant@kobo.com',
         date: new Date(),
         amount: 15000
+    }, {
+        id: 2,
+        investor: 'Juan Barinas',
+        email_address: 'jbarinas@gmail.com',
+        date: new Date(),
+        amount: 37000
+    }, {
+        id: 3,
+        investor: 'Adam Lemmon',
+        email_address: 'alemmon@gmail.com',
+        date: new Date(),
+        amount: 100000
+    }, {
+        id: 4,
+        investor: 'Charlie Morris',
+        email_address: 'cmorris@gmail.com',
+        date: new Date(),
+        amount: 74789
     }
 ]
 
