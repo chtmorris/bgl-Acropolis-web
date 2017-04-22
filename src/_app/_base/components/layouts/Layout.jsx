cbdd4e45675180be56e85c0bd3cb1f42cc7ab885
jbarinas <jbarinas@kobo.com>
2017-04-22 10:15:27 -0400

    fix(layout)

--- src/_app/_base/components/layouts/Layout.jsx
+++ src/_app/_base/components/layouts/Layout.jsx
@@ -4,21 +4,17 @@ export default class Layout extends React.Component {
     render() {
         return (
             <app-drawer-layout fullbleed>
-                <app-drawer align="right">{this.props.nav}</app-drawer>
+                {/*
+                    <app-drawer align="right">{this.props.nav}</app-drawer>
+                */}
 
                 <app-header-layout class="layout vertical" fullbleed>
-                    {(() => {
-                        if (this.props.title) {
-                            return (
-                                <app-header>
-                                    <app-toolbar>
-                                        <div>{this.props.title}</div>
-                                        <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
-                                    </app-toolbar>
-                                </app-header>
-                            );
-                        }
-                    })()}
+                    <app-header>
+                        <app-toolbar>
+                            <div>ACROPOLIS</div>
+                            <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
+                        </app-toolbar>
+                    </app-header>
 
                     <div className="relative flex">
                         <main className="fit">
